21ca8b2ca6daadbec0ea881dae80e6c6fadc0ff6
Anne Fouilloux <annef@simula.no>
2026-03-19 17:45:29 +0100

    fix lint error

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { fileURLToPath, URL } from "url";
 
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm()],
+  plugins: [vue(), wasm(), topLevelAwait()],
   build: {
     sourcemap: true,
+    target: "es2022",
   },
   server: {
     proxy: {
